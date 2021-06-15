4b81737c586d5fc6abcb3199044ed56e99dfabf0
sunil goli <gsunil.mca@gmail.com>
2021-06-15 16:06:32 +0000

    configuring app

--- src/main/java/io/pivotal/pal/tracker/PalTrackerApplication.java
+++ src/main/java/io/pivotal/pal/tracker/PalTrackerApplication.java
@@ -1,4 +1,4 @@
-package io.pivotal.pal.traker;
+package io.pivotal.pal.tracker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,7 +6,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class PalTrackerApplication {
 
+
     public static void main(String[] args) {
+
         SpringApplication.run(PalTrackerApplication.class, args);
     }
 }
